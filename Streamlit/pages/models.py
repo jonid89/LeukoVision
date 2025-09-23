57c39b70828d500dbe0aa7589c1aee5c8643803d
ahmedali42 <ahmedali.4290@gmail.com>
2025-09-23 16:22:45 +0200

    update

--- Streamlit/pages/models.py
+++ Streamlit/pages/models.py
@@ -92,7 +92,53 @@ Grad-CAM visualization shows that the model consistently focuses on the cell its
                  caption='Four randomly selected test set images with their ground-truth '
                  'and predicted labels. (Bottom) Corresponding Grad-CAM visualizations highlighting ' \
                  'the image regions most influential in the model’s predictions.',use_container_width=True)
-    
+    st.markdown('### Performace')
+    st.markdown(""" <div style="text-align: justify;">
+    The training accuracy approaches 99.97%, while the validation accuracy reaches 
+    98.29%, demonstrating the model’s strong ability to accurately classify different cell types.
+                </div>
+                """,unsafe_allow_html=True)
+    if st.toggle("Show InceptionV3 loss and accuracy plot"):
+        st.image(white_bg('./Streamlit/pages/images/inception/loss_acc.png'), caption='Loss and accruacy plot from InceptionV3 training',use_container_width=True)
+    st.markdown(""" <div style="text-align: justify;">
+                The test set shows a very high accuracy of 98.28% 
+                and that is reflected in the digonal form of the confusion matrix.
+                </div>
+                """,unsafe_allow_html=True)
+    if st.toggle("Show InceptionV3 confusion matrix"):
+        st.image(white_bg('./Streamlit/pages/images/inception/cm.png'), caption='Confusion matrix of InceptionV3 test set',use_container_width=True)
+    report = pd.read_csv("./Streamlit/pages/images/inception/class_report.txt", 
+                     sep="\s+", header=0,
+                     names=["Class", "Recall", "Specificity", "Precision", "F1-Score"])
+    report.index = report.index + 1
+    numeric_cols = report.select_dtypes(include="number").columns
+    styled = report.style.format({col: "{:.2f}" for col in numeric_cols}) \
+                        .set_properties(**{"text-align": "center"}) \
+                        .set_table_styles([{
+                            "selector": "th",
+                            "props": [("text-align", "center"), ("font-weight", "bold")]
+                        }])
+    st.markdown(""" <div style="text-align: justify;">
+                The classification metrics consistently range between 0.96 and 1.00, 
+                demonstrating that the InceptionV3 model performs exceptionally well in 
+                distinguishing among different WBC subtypes. This highlights both the robustness of 
+                the model and its suitability for automated cell classification tasks.
+                </div>
+                """,unsafe_allow_html=True)
+    if st.toggle("Show classification report"):
+        st.dataframe(styled)
+    st.markdown('### Interpretability by Grad-CAM')
+    st.markdown(""" <div style="text-align: justify;">
+Grad-CAM visualization shows that the model consistently focuses on the cell itself while ignoring the 
+                background, suggesting that the classification is based on cytomorphological features 
+                of the cells rather than irrelevant image artifacts.
+                </div>
+                """,unsafe_allow_html=True)
+    if st.toggle("Show InceptionV3 Grad-CAM results"):
+        st.image(white_bg('./Streamlit/pages/images/inception/prediction.png'), 
+                 caption='Four randomly selected test set images with their ground-truth '
+                 'and predicted labels. (Bottom) Corresponding Grad-CAM visualizations highlighting ' \
+                 'the image regions most influential in the model’s predictions.',use_container_width=True)
 
 # The key idea behind InceptionV3 is the use of **Inception modules**, which allow the network to capture features at multiple scales simultaneously. Each module applies several convolutions of different sizes in parallel and concatenates the results, enabling the model to learn both fine and coarse features from an image.  
 
