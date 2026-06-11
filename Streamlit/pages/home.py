3d10bd7c1f79a569392a00f63be96c58c40dbfc2
David Poussif <david.poussif@gmail.com>
2026-06-11 11:02:11 +0200

    Updated Home UI

--- Streamlit/pages/home.py
+++ Streamlit/pages/home.py
@@ -8,7 +8,7 @@ with st.container():
     st.markdown("""
     <div style="text-align: justify;">
                 
-    **LeukoVision** is an advanced AI-powered platform designed to analyze and classify blood cell images with precision.  
+    **LeukoVision** is an advanced AI-powered platform designed to analyze and classify blood cell images with precision. Use the [🧪Vision Lab](visionlab) page to get a prediction!
     Leveraging state-of-the-art deep learning models — **InceptionV3**, **ResNet50**, and **VGG16** — the app provides accurate identification of various blood cell types, including basophils, eosinophils, erythroblasts, lymphocytes, monocytes, neutrophils, and platelets.
 
     With **LeukoVision**, you can: 
@@ -20,3 +20,15 @@ with st.container():
     This interactive tool is perfect for **researchers, educators, and medical professionals** looking to combine computational pathology with intuitive visualization.
     </div>
     """, unsafe_allow_html=True)
+
+st.write("") # Add a bit of spacing
+
+# Display the 3 images in 3 aligned columns
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.image('./Streamlit/gallery/Image processing/Image processing-1.png', use_container_width=True)
+with col2:
+    st.image('./Streamlit/gallery/Image processing/Image processing-2.png', use_container_width=True)
+with col3:
+    st.image('./Streamlit/gallery/Image processing/Image processing-3.png', use_container_width=True)
